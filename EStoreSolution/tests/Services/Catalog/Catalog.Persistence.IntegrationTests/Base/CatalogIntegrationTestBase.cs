74755f6e2b811a5fd0dbefcdc0e4fc552a540e83
Valery Kalasouski <valerykalasouski@gmail.com>
2024-10-21 10:49:52 +0200

    added tests for persistence layer

---
+++ EStoreSolution/tests/Services/Catalog/Catalog.Persistence.IntegrationTests/Base/CatalogIntegrationTestBase.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Persistence.IntegrationTests.Base
+{
+	public class CatalogIntegrationTestBase
+	{
+		protected readonly ApplicationDbContext CatalogContext;
+
+		public CatalogIntegrationTestBase()
+		{
+			var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.Test.json").Build();
+
+			bool useInMemoryDatabase = configuration.GetValue<bool>("UseInMemoryDatabase");
+
+			var serviceCollection = new ServiceCollection();
+			if (useInMemoryDatabase)
+			{
+				serviceCollection.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("InMemoryCatalogTestDb"));
+			}
+			else
+			{
+				serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlServer(configuration.GetConnectionString("DatabaseSettings:ConnectionString")));
+			}
+
+			var serviceProvider = serviceCollection.BuildServiceProvider();
+			var scope = serviceProvider.CreateScope();
+			CatalogContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+			if (!useInMemoryDatabase)
+			{
+				CatalogContext.Database.Migrate();
+			}
+		}
+
+		protected void CleanDatabase()
+		{
+			CatalogContext.Categories.RemoveRange(CatalogContext.Categories);
+			CatalogContext.Products.RemoveRange(CatalogContext.Products);
+			CatalogContext.SaveChanges();
+		}
+	}
+}
---
+++ EStoreSolution/tests/Services/Catalog/Catalog.Persistence.IntegrationTests/ProductRepositoryIntegrationTests.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using Catalog.Domain.Entities;
+using Catalog.Persistence.IntegrationTests.Base;
+
+namespace Catalog.Persistence.IntegrationTests
+{
+	public class ProductRepositoryIntegrationTests : CatalogIntegrationTestBase
+	{
+		public ProductRepositoryIntegrationTests()
+		{
+			CleanDatabase();
+		}
+
+		[Fact]
+		public async Task AddProduct_ShouldAddProductToDatabase()
+		{
+			// Arrange
+			var product = new Product
+			{
+				Id = 1,
+				Name = "Test Product",
+				Image = "https://example.com/default-image.png",
+				Description = "Description",
+				Price = 10.99m,
+				Amount = 5,
+				CategoryId = 1,
+				Category = new Category
+				{
+					Id = 1,
+					Name = "Test Category"
+				}
+			};
+
+			// Act
+			await CatalogContext.Products.AddAsync(product);
+			await CatalogContext.SaveChangesAsync();
+
+			// Assert
+			var insertedProduct = await CatalogContext.Products.FindAsync(product.Id);
+			insertedProduct.Should().NotBeNull();
+			insertedProduct!.Name.Should().Be("Test Product");
+		}
+
+		[Fact]
+		public async Task GetProduct_ShouldReturnProduct_WhenProductExists()
+		{
+			// Arrange
+			var product = new Product
+			{
+				Id = 1,
+				Name = "Existing Product",
+				Image = "https://example.com/default-image.png",
+				Description = "Description",
+				Price = 15.99m,
+				Amount = 10,
+				CategoryId = 1,
+				Category = new Category
+				{
+					Id = 1,
+					Name = "Test Category"
+				}
+			};
+			await CatalogContext.Products.AddAsync(product);
+			await CatalogContext.SaveChangesAsync();
+
+			// Act
+			var retrievedProduct = await CatalogContext.Products.FindAsync(product.Id);
+
+			// Assert
+			retrievedProduct.Should().NotBeNull();
+			retrievedProduct!.Name.Should().Be("Existing Product");
+		}
+
+		[Fact]
+		public async Task DeleteProduct_ShouldRemoveProductFromDatabase()
+		{
+			// Arrange
+			var product = new Product
+			{
+				Id = 1,
+				Name = "Product to delete",
+				Image = "https://example.com/default-image.png",
+				Description = "Description",
+				Price = 8.99m,
+				Amount = 1,
+				CategoryId = 1,
+				Category = new Category
+				{
+					Id = 1,
+					Name = "Test Category"
+				}
+			};
+			await CatalogContext.Products.AddAsync(product);
+			await CatalogContext.SaveChangesAsync();
+
+			// Act
+			CatalogContext.Products.Remove(product);
+			await CatalogContext.SaveChangesAsync();
+
+			// Assert
+			var deletedProduct = await CatalogContext.Products.FindAsync(product.Id);
+			deletedProduct.Should().BeNull();
+		}
+	}
+}
